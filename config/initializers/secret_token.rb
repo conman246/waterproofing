c7867262d8b0dfaadde171876a4005331a41df8f
connor McCrone <your.email@example.com>
2014-02-17 17:51:02 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Waterproofing::Application.config.secret_key_base = 'b3781ba33f0f587ed7ff3d8ec4ebc336df269d650cbf641852b70d8e572e511e7b4918ff2d1a23eaf4e144e8ca2fd9e29761e565dce9ef5bfcfbdf2ec85c8997'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Waterproofing::Application.config.session_store :cookie_store, key: '_waterproofing_session'
